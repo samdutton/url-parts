b9c161cac74c88200052313eb92b2697771a20da
Glitch (address-form) <none>
2023-02-07 19:49:55 +0000

    🍋💄 Checkpoint ./js/main.js:111157/11116 ./css/main.css:111157/10066 ./index.html:111157/4222

--- js/main.js
+++ js/main.js
@@ -7,10 +7,12 @@ import psl from './psl.js';
 const urlInput = document.querySelector('input#url');
 const urlPartsDiv = document.querySelector('div#url-parts');
 
-const searchParams = new URLSearchParams(window.location.search);
-const urlParam = searchParams.get('url');
+
+// const searchParams = new URLSearchParams(window.location.search);
+// const urlParam = searchParams.get('url');
+// Get the value 'manually' to allow for hash values.
+const urlParam = location.href.split('?url=')[1];
 if (urlParam) {
-  console.log(urlParam);
   urlInput.value = urlParam;
   handleUrl();
 }
@@ -19,13 +21,18 @@ urlInput.oninput = handleUrl;
 
 function handleUrl() {
   const urlText = urlInput.value;
-  console.log('urlText', urlText);
+  // location.href = `${location.host}/?url=${urlText}`;
 
   // URL API allows URLs such as `https://foo` or `https://f`.
   // Also want to avoid URLs like `//foo` or `foo.co.`.
   // URLs like `tv.tv` are valid.
-  if (!urlText || !urlText.match(/\w{2,}\.\w{2,}/) || urlText.match(/$\./)) {
+  if (!urlText || !urlText.match(/\w{2,}\.\w{2,}/) || urlText.match(/$\./ )) {
     urlPartsDiv.innerHTML = '';
+    return;
+  }
+
+  if (urlText.match(/\s/)) {
+    urlPartsDiv.innerHTML = 'URL includes a space.';
     return;
   }
 
@@ -89,8 +96,8 @@ function handleUrl() {
     etld1 = urlText.match(etld1RegExp) && urlText.match(etld1RegExp)[0];
   }
 
-  // Add span for the origin.
-  // Span hierarchy: hostname (site) > eTLD+1 > eTLD > TLD.
+  // The spans need to wrap the URL from the outside in:
+  // origin > hostname > site > eTLD+1 > eTLD > TLD.
   urlPartsDiv.innerHTML = urlText.replace(origin,
     `<span id="origin">${origin}</span>`);
 
@@ -101,8 +108,19 @@ function handleUrl() {
   if (etld) {
     urlPartsDiv.innerHTML = urlPartsDiv.innerHTML.replace(etld1,
       `<span id="etld1">${etld1}</span>`);
+    // Site now requires scheme.
+    // TODO: move down origin and hostname spans if there is no site span.
+    if (scheme) {
+      urlPartsDiv.innerHTML = urlPartsDiv.innerHTML.replace(`<span id="etld1">${etld1}</span>`,
+        `<span id="etld1"><span id="site">${etld1}</span></span>`);
+    }
     urlPartsDiv.innerHTML = urlPartsDiv.innerHTML.replace(etld,
       `<span id="etld">${etld}</span>`);
+  // Site now requires scheme.
+  } else if (scheme) {
+    // Not eTLD, so site is TLD+1.
+    const site = hostname.split('.').slice(-2).join('.');
+    urlPartsDiv.innerHTML = urlPartsDiv.innerHTML.replace(site, `<span id="site">${site}</span>`);
   }
 
   // Wrap TLD in a span.
@@ -136,15 +154,14 @@ function handleUrl() {
     urlPartsDiv.innerHTML = urlPartsDiv.innerHTML.replace(hash,
       `<span id="hash">${hash}</span>`);
   }
-  console.log('port, pathname before', port, pathname);
   if (port) {
-    console.log('port, pathname inside', port, pathname);
     urlPartsDiv.innerHTML = urlPartsDiv.innerHTML.replace(`:${port}`,
       `:<span id="port">${port}</span>`);
   }
   if (scheme) {
     urlPartsDiv.innerHTML = urlPartsDiv.innerHTML.replace(scheme,
       `<span id="scheme">${scheme}</span>`);
+    // `<span id="scheme"><span id="site-scheme">${scheme}</span></span>`);
   }
   // If the URL has a hash value *and* a search string,
   // the URL API (for hash) returns the hash and the search string.
@@ -153,3 +170,4 @@ function handleUrl() {
       `<span id="search">${search}</span>`);
   }
 };
+
